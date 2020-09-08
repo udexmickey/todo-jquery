1a3c42e78eb92fc327d01681a6b49fa8d447015f
Dimgba Micheal <jerrymike80@yahoo.com>
2020-09-08 17:19:26 +0100

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+//the section that marks through each list item when clicked upon.
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("complete");
+});
+
+//this is the code behind deleting your Todos from the list.
+$("li").on("click", "span", function(event){
+    $(this).parent().fadeOut(function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").on("keypress", function(event){
+   var scores = $(this).val();
+   
+   if(event.which === 13){
+    $(this).val("");
+    $("ul").append("<li><span>X</span> "+ scores  +" </li>")
+   }
+});
